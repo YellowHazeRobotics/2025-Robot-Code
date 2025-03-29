41f22dc42b596f64808be4b31ea3d15936f44678
Darren Lau <dlau@yellowhazerobotics.org>
2025-03-29 19:05:38 -0400

    Algae Sub added

--- src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
@@ -4,14 +4,92 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AlgaeConstants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class AlgaeSubsystem extends SubsystemBase {
-  /** Creates a new AlgaeSubsystem. */
-  public AlgaeSubsystem() {}
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+public class AlgaeSubsystem extends SubsystemBase {
+  
+  private SparkMax m_Algae;
+  SparkMaxConfig algaeConfig;
+
+  private double currentTargetPosition;
+
+  private RelativeEncoder encoder;
+  private SparkClosedLoopController feedbackController;
+
+  public AlgaeSubsystem() {
+    m_Algae = new SparkMax(AlgaeConstants.kAlgaeMotorID, MotorType.kBrushless);
+
+    algaeConfig = new SparkMaxConfig();
+
+    configureMotors();
+
+    encoder = m_Algae.getEncoder();
+    feedbackController = m_Algae.getClosedLoopController();
+  }
+
+  public void configureMotors(){
+    algaeConfig
+    .inverted(AlgaeConstants.kInverted)
+    .idleMode(AlgaeConstants.kIdleMode);
+
+    m_Algae.configure(algaeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  public Command spinAlgaeMotors(double position) {
+    return runOnce(() -> {
+      currentTargetPosition = position;
+      feedbackController.setReference(position, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    });
+  }
+
+  public Command resetEncoder() {
+    return runOnce(() -> {
+            encoder.setPosition(0);
+        });
+}
+
+public void stopMotorManual() {
+  m_Algae.set(0);
+}
+
+public Command manualForward(){
+  return startEnd(
+    () -> m_Algae.set(0.5),
+    () -> m_Algae.set(0));
+}
+
+public Command manualBackWard(){
+  return startEnd(
+    () -> m_Algae.set(-0.5),
+    () -> m_Algae.set(0));
+}
+@Override
+public void periodic() {
+    SmartDashboard.putNumber("Algae Intake Position", encoder.getPosition());
+  }
+
+@Override
+public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
   }
 }
